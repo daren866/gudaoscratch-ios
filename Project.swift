b675f24f99beb0213d0255874c74ae59ed5ec7d4
daren866 <xiao1xiao188@qq.com>
2026-06-19 14:24:12 +0800

    Initial commit

---
+++ Project.swift
@@ -0,0 +1,30 @@
+import ProjectDescription
+
+let deploymentTargets: DeploymentTargets = .iOS("15.0")
+
+let project = Project(
+    name: "Gudaoscratch",
+    organizationName: "Example",
+    settings: .settings(
+        base: [
+            "DEVELOPMENT_TEAM": "<YOUR_DEVELOPMENT_TEAM_ID>",
+            "CODE_SIGN_STYLE": "Automatic",
+            "CODE_SIGN_IDENTITY": "Apple Development",
+            "TARGETED_DEVICE_FAMILY": "1",
+            "ENABLE_USER_SCRIPT_SANDBOXING": "NO"
+        ],
+        defaultSettings: .recommended
+    ),
+    targets: [
+        .target(
+            name: "Gudaoscratch",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "com.gudao.scratch",
+            deploymentTargets: deploymentTargets,
+            infoPlist: .file(path: "Gudaoscratch/Info.plist"),
+            sources: ["Gudaoscratch/**/*.swift"],
+            resources: ["Gudaoscratch/Assets.xcassets"]
+        )
+    ]
+)
